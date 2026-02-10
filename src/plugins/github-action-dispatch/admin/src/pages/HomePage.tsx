e35cb6307826cfdd2a1405d8b18827ab2a548933
erkobridee <erko.bridee@gmail.com>
2026-02-10 17:07:09 +0100

    update(github-action-dispatch plugin):  HomePage component

--- src/plugins/github-action-dispatch/admin/src/pages/HomePage.tsx
+++ src/plugins/github-action-dispatch/admin/src/pages/HomePage.tsx
@@ -1,14 +1,45 @@
-import { Main } from '@strapi/design-system';
 import { useIntl } from 'react-intl';
 
-import { getTranslation } from '../utils/getTranslation';
+import {
+  Main,
+
+  // https://design-system.strapi.io/?path=/docs/components-box--docs
+  Box,
+
+  // https://design-system.strapi.io/?path=/docs/foundations-typography--docs
+  Typography
+} from '@strapi/design-system';
+
+// import { getTranslation } from '../utils/getTranslation';
+
+//----------------------------------------------------------------------------//
 
 const HomePage = () => {
   const { formatMessage } = useIntl();
 
+  /*
   return (
-    <Main>
-      <h1>Welcome to {formatMessage({ id: getTranslation('plugin.name') })}</h1>
+    <Main padding={5}>
+      <Box paddingBottom={4} margin={20}>
+        <Typography variant="alpha">Welcome to {formatMessage({ id: getTranslation('plugin.name') })}</Typography>
+
+        <Box>
+          <Typography variant="epsilon">{formatMessage({ id: getTranslation('plugin.description') })}</Typography>
+        </Box>
+      </Box>
+    </Main>
+  );
+  */
+
+  return (
+    <Main padding={5}>
+      <Box paddingBottom={4} margin={20}>
+        <Typography variant="alpha">Welcome to {`GitHub Action Dispatch`}</Typography>
+
+        <Box>
+          <Typography variant="epsilon">{`Make it possible to dispatch an event into a specific GitHub repository`}</Typography>
+        </Box>
+      </Box>
     </Main>
   );
 };
