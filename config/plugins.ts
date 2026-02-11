686c4943c5b6999e01bd96a698e5219bd4fba776
erkobridee <erko.bridee@gmail.com>
2026-02-11 19:11:21 +0100

    update(plugins config): only config the cache if the CACHE_PORT env var is defined

--- config/plugins.ts
+++ config/plugins.ts
@@ -4,66 +4,72 @@
 // Redis Configuration
 // https://strapi-community.github.io/plugin-rest-cache/guide/provider/redis.html
 
-export default ({ env }) => ({
-  ckeditor5: { enabled: true },
+export default ({ env }) => {
+  const isCacheEnabled = !!env("CACHE_PORT");
 
-  // https://docs.strapi.io/cms/plugins-development/create-a-plugin#configuration-with-a-local-plugin
-  "github-action-dispatch": {
-    enabled: true,
-    resolve: "./src/plugins/github-action-dispatch",
-    config: {
-      token: env('GITHUB_PAT'),
-      url: env('GITHUB_URL'),
-      eventType: env('GITHUB_EVENT_TYPE')
-    }
-  },
+  return {
+    ckeditor5: { enabled: true },
 
-  // https://github.com/strapi-community/plugin-redis
-  redis: {
-    config: {
-      settings: {
-        debug: false,
-        enableRedlock: true,
+    // https://docs.strapi.io/cms/plugins-development/create-a-plugin#configuration-with-a-local-plugin
+    "github-action-dispatch": {
+      enabled: true,
+      resolve: "./src/plugins/github-action-dispatch",
+      config: {
+        token: env("GITHUB_PAT"),
+        url: env("GITHUB_URL"),
+        eventType: env("GITHUB_EVENT_TYPE"),
       },
-      connections: {
-        default: {
-          connection: {
-            host: '127.0.0.1',
-            port: env('CACHE_PORT', 6379),
-            db: 0,
-          },
-          settings: {
-            debug: false,
+    },
+
+    // https://github.com/strapi-community/plugin-redis
+    redis: {
+      enabled: isCacheEnabled,
+      config: {
+        settings: {
+          debug: false,
+          enableRedlock: true,
+        },
+        connections: {
+          default: {
+            connection: {
+              host: "127.0.0.1",
+              port: env("CACHE_PORT", 6379),
+              db: 0,
+            },
+            settings: {
+              debug: false,
+            },
           },
         },
       },
     },
-  },
 
-  "rest-cache": {
-    config: {
-      provider: {
-        name: "redis",
+    "rest-cache": {
+      enabled: isCacheEnabled,
+      config: {
+        provider: {
+          name: "redis",
 
-        options: {
-          // The name of the connection as defined in the Redis plugin.
-          connection: "default",
+          options: {
+            // The name of the connection as defined in the Redis plugin.
+            connection: "default",
 
-          // The time to live in milliseconds. This is the maximum amount of time that an item can be in the cache before it is removed.
-          ttl: 3600 * 1000,
+            // The time to live in milliseconds. This is the maximum amount of time that an item can be in the cache before it is removed.
+            ttl: 3600 * 1000,
+          },
+        },
+        strategy: {
+          // if you are using keyPrefix for your Redis, please add <keysPrefix>
+          // keysPrefix: "<redis_keyPrefix>",
+
+          contentTypes: [
+            // list of Content-Types UID to cache
+            "api::tag.tag",
+            "api::blog.blog",
+            "api::page.page",
+          ],
         },
       },
-      strategy: {
-        // if you are using keyPrefix for your Redis, please add <keysPrefix>
-        // keysPrefix: "<redis_keyPrefix>",
-
-        contentTypes: [
-          // list of Content-Types UID to cache
-          "api::tag.tag",
-          "api::blog.blog",
-          "api::page.page",
-        ],
-      },
     },
-  },
-});
+  };
+};
